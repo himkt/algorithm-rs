e1f1890ba6095fa272083b1b689ad566926dcb96
Makoto Hiramatsu <himkt@cookpad.com>
2021-06-06 23:32:45 +0900

    Add combination

---
+++ src/misc/combination.rs
@@ -0,0 +1,39 @@
+pub fn combination(n: i64, mut r: i64) -> i64 {
+    if r > n {
+        return 0;
+    }
+
+    if r * 2 > n {
+        r = n - r;
+    }
+
+    if r == 0 {
+        return 1;
+    }
+
+    let mut res = 1;
+    for i in 1..(r+1) {
+        res *= n - i + 1;
+        res /= i;
+    }
+
+    res
+}
+
+
+#[cfg(test)]
+mod test_combination {
+    #[test]
+    fn it_works() {
+        use crate::misc::combination::combination;
+        {
+            assert_eq!(combination(6, 0), 1);
+            assert_eq!(combination(6, 1), 6);
+            assert_eq!(combination(6, 2), 15);
+            assert_eq!(combination(6, 3), 20);
+            assert_eq!(combination(6, 4), 15);
+            assert_eq!(combination(6, 5), 6);
+            assert_eq!(combination(6, 6), 1);
+        }
+    }
+}
