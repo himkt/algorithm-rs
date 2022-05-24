130d8f0ad3e03033d3caf02f18467f0def8b7d35
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-24 21:12:13 +0900

    Generic implementation

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -1,5 +1,3 @@
 pub mod convex_hull;
-pub mod float_line;
-pub mod float_point;
-pub mod int_line;
-pub mod int_point;
+pub mod point;
+pub mod line;
---
+++ src/geometry/point.rs
@@ -0,0 +1,40 @@
+use std::ops::{Add, Sub, Mul, Div};
+
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Point<T>(pub T, pub T);
+
+
+impl<T: Add<T, Output=T>> std::ops::Add<Point<T>> for Point<T> {
+    type Output = Point<T>;
+    fn add(self, _rhs: Point<T>) -> Self::Output {
+        Point(self.0 + _rhs.0, self.1 + _rhs.1)
+    }
+}
+
+impl<T: Sub<T, Output=T>> std::ops::Sub<Point<T>> for Point<T> {
+    type Output = Point<T>;
+    fn sub(self, _rhs: Point<T>) -> Self {
+        Point(self.0 - _rhs.0, self.1 - _rhs.1)
+    }
+}
+
+impl<T: Copy + Mul<T, Output=T>> std::ops::Mul<T> for Point<T> {
+    type Output = Point<T>;
+    fn mul(self, k: T) -> Self {
+        Self(k * self.0, k * self.1)
+    }
+}
+
+impl<T: Copy + Div<T, Output=T>> std::ops::Div<T> for Point<T> {
+    type Output = Self;
+    fn div(self, k: T) -> Self {
+        Self(self.0 / k, self.1 / k)
+    }
+}
+
+impl<T: Mul<T, Output=T> + Add<T, Output=T>> Point<T> {
+    pub fn det(self, _rhs: Point<T>) -> T {
+        self.0 * _rhs.1 + self.1 * _rhs.0
+    }
+}
