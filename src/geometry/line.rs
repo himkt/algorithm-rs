f1ea9c6b2b3bae268d6cff83d49e0ae12dc80199
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-25 00:07:50 +0900

    Cleanup trait

--- src/geometry/line.rs
+++ src/geometry/line.rs
@@ -1,14 +1,15 @@
 use crate::geometry::point::Point;
 
-
 pub struct Line<T>(pub Point<T>, pub Point<T>);
 
 
-pub trait LineDistance<T> {
+pub trait LineAPI<T> {
     fn distance(&self, p: Point<T>) -> f64;
+    fn contains_point(&self, p: Point<T>) -> bool;
 }
 
-impl LineDistance<i64> for Line<i64> {
+// i64
+impl LineAPI<i64> for Line<i64> {
     fn distance(&self, p: Point<i64>) -> f64 {
         let v1 = self.1 - self.0;
         let v2 = p - self.0;
@@ -16,9 +17,16 @@ impl LineDistance<i64> for Line<i64> {
         let det = v1.det(v2) as f64;
         det / l1
     }
+
+    fn contains_point(&self, p: Point<i64>) -> bool {
+        let d = self.1 - self.0;
+        let e = p - self.0;
+        d.1 * e.0 == d.0 * e.1
+    }
 }
 
-impl LineDistance<f64> for Line<f64> {
+// f64
+impl LineAPI<f64> for Line<f64> {
     fn distance(&self, p: Point<f64>) -> f64 {
         let v1 = self.1 - self.0;
         let v2 = p - self.0;
@@ -26,13 +34,7 @@ impl LineDistance<f64> for Line<f64> {
         let det = v1.det(v2);
         det / l1
     }
-}
 
-pub trait LineContain<T> {
-    fn contains_point(&self, p: Point<T>) -> bool;
-}
-
-impl LineContain<f64> for Line<f64> {
     fn contains_point(&self, p: Point<f64>) -> bool {
         let d = self.1 - self.0;
         let e = p - self.0;
@@ -45,8 +47,7 @@ impl LineContain<f64> for Line<f64> {
 mod test_plane {
     use crate::geometry::line::Line;
     use crate::geometry::line::Point;
-    use crate::geometry::line::LineDistance;
-    use crate::geometry::line::LineContain;
+    use crate::geometry::line::LineAPI;
 
     #[test]
     fn it_works_line_f64() {
@@ -55,33 +56,34 @@ mod test_plane {
 
         let line = Line(p1, p2);
 
-        let p3 = Point(0.0, 3.0);
-        let d = line.distance(p3);
+        let p1 = Point(0.0, 3.0);
+        let d = line.distance(p1);
         let d_truth = 1.664_100_588_675_687_4;
 
         let i1 = (d * 1_000_000_000.0) as i64;
         let i2 = (d_truth * 1_000_000_000.0) as i64;
         assert_eq!(i1, i2);
-
-        let line = Line(p1, p2);
-
-        assert!(line.contains_point(Point(4.0,  6.0)));
-        assert!(line.contains_point(Point(-2.0, -3.0)));
-        assert!(!line.contains_point(Point(1.0, 1.0)));
     }
 
     #[test]
     fn it_works_line_i64() {
-        let p3 = Point(0, 0);
-        let p4 = Point(2, 3);
-        let line = Line(p3, p4);
+        let p1 = Point(0, 0);
+        let p2 = Point(2, 3);
+        let line = Line(p1, p2);
 
-        let p5 = Point(0, 3);
-        let d = line.distance(p5);
+        let p3 = Point(0, 3);
+        let d = line.distance(p3);
         let d_truth = 1.664_100_588_675_687_4;
 
         let i3 = (d * 1_000_000_000.0) as i64;
         let i4 = (d_truth * 1_000_000_000.0) as i64;
         assert_eq!(i3, i4);
+
+
+        let line = Line(p1, p2);
+
+        assert!(line.contains_point(Point(4,  6)));
+        assert!(line.contains_point(Point(-2, -3)));
+        assert!(!line.contains_point(Point(1, 1)));
     }
 }
