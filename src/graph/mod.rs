c3f07dce4208eae2b58807582ea9786aefa3dd19
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-28 08:53:15 +0900

    clippy::module_inception

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,8 +1,9 @@
 pub mod bfs;
 pub mod dfs;
 pub mod dijkstra;
+#[allow(clippy::module_inception)]
+pub mod graph;
 pub mod lowlink;
 pub mod topological_sort;
 pub mod scc;
 pub mod lca;
-pub mod graph;
