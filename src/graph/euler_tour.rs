f3966ecd7b7f81b9b875ee9779b0f7f29b273681
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-06-11 18:15:05 +0900

    Implement euler tour

---
+++ src/graph/euler_tour.rs
@@ -0,0 +1,67 @@
+use crate::graph::graph::Graph;
+
+const INF: usize = 1_000_000_000_000;
+
+pub struct EulerTour {
+    graph: Graph,
+    l: Vec<usize>,
+    r: Vec<usize>,
+    t: usize,
+}
+
+impl EulerTour {
+    pub fn new(n: usize, graph: Graph) -> Self {
+        let l = vec![INF; n];
+        let r = vec![INF; n];
+        EulerTour { graph, l, r, t: 1 }
+    }
+
+    pub fn traverse(&mut self, root: usize) -> (&[usize], &[usize]) {
+        self.dfs(root, None);
+
+        for i in 0..self.l.len() {
+            if self.r[i] == INF {
+                self.r[i] = self.l[i];
+            }
+        }
+
+        (&self.l, &self.r)
+    }
+
+    pub fn dfs(&mut self, u: usize, p: Option<usize>) {
+        self.l[u] = self.t;
+        self.t += 1;
+
+        for i in 0..self.graph.graph[u].len() {
+            let (v, _) = self.graph.graph[u][i];
+            if p != Some(v) {
+                self.dfs(v, Some(u));
+                self.r[u] = self.t;
+                self.t += 1;
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod test_euler_tour {
+    use crate::graph::graph::Graph;
+    use crate::graph::euler_tour::EulerTour;
+
+    #[test]
+    fn it_works() {
+        let mut graph = Graph::new(7, false);
+        graph.connect_unweighted(0, 1);
+        graph.connect_unweighted(1, 2);
+        graph.connect_unweighted(1, 3);
+        graph.connect_unweighted(3, 4);
+        graph.connect_unweighted(0, 5);
+        graph.connect_unweighted(5, 6);
+
+        let mut euler_tour = EulerTour::new(7, graph);
+        let (l, r) = euler_tour.traverse(0);
+
+        assert_eq!(l, &vec![1, 2, 3, 5, 6, 10, 11]);
+        assert_eq!(r, &vec![13, 8, 3, 7, 6, 12, 11]);
+    }
+}
