6d939b4d6d71db519ec07f2715ea6d7edd75e17b
himkt <himkt@users.noreply.github.com>
2024-01-10 10:24:42 +0000

    deploy: 3110865efd67454ff64c6f4734f706e389ac9e70

--- implementors/core/cmp/trait.Ord.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"algorithm_rs":[["impl&lt;T: <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.74.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"algorithm_rs/geometry/point/struct.Point.html\" title=\"struct algorithm_rs::geometry::point::Point\">Point</a>&lt;T&gt;"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
